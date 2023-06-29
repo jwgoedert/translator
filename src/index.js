b506c0c06354747ad6cdab613c1612ac5885d84f
James Wesley Goedert <jwgoedert@gmail.com>
2023-06-29 13:32:15 -0500

    removed unecessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//1 Import react and React DOM libraries
+//2 get a reference to the div with ID root
+//3 Tell react to take control fo that element
+//4 Create a component 
+//5 Show component on the screen
